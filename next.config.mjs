92597754dd31bf8ee0eb7d25f7e3219a07409068
2192008 <fw.jay08@gmail.com>
2025-10-26 01:34:54 -0800

    Quick Docs GUI UI Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'docs.vunsh.xyz',
-          },
-        ],
-        destination: '/docs/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
