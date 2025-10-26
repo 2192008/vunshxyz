b6ff25542360e39fdb2e7415e4df4493f0305b81
2192008 <fw.jay08@gmail.com>
2025-10-25 22:25:23 -0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,24 +5,6 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/boobismp',
-        destination: 'https://BoobiSMP.aternos.me',
-        permanent: false,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'boobismp.vunsh.xyz',
-          },
-        ],
-        destination: 'https://BoobiSMP.aternos.me/:path*',
-        permanent: true,
-      },
-
-      // Redirect docs.vunsh.xyz to external docs site
-      {
         source: '/:path*',
         has: [
           {
